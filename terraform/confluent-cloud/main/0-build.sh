83099ce943965b3849529c4348b17b0d8636ba03
Warren Vella <warren.vella18@gmail.com>
2025-06-06 12:58:46 +1000

    Bump to v1.0.1

--- terraform/confluent-cloud/main/0-build.sh
+++ terraform/confluent-cloud/main/0-build.sh
@@ -15,8 +15,8 @@ make build SERVICE=byte-to-eat-v2-docker-consumer-recipes IMAGE_NAME=byte-to-eat
 make docker-push SERVICE=byte-to-eat-v2-docker-consumer-recipes IMAGE_NAME=byte-to-eat-docker-consumer-recipes IMAGE_TAG=2.0.0
 
 # Producer Orders
-make build SERVICE=byte-to-eat-v1-docker-producer-orders IMAGE_NAME=byte-to-eat-docker-producer-orders
-make docker-push SERVICE=byte-to-eat-v1-docker-producer-orders IMAGE_NAME=byte-to-eat-docker-producer-orders
+make build SERVICE=byte-to-eat-v1-docker-producer-orders IMAGE_NAME=byte-to-eat-docker-producer-orders IMAGE_TAG=1.0.1
+make docker-push SERVICE=byte-to-eat-v1-docker-producer-orders IMAGE_NAME=byte-to-eat-docker-producer-orders IMAGE_TAG=1.0.1
 
 # Consumer Orders
 make build SERVICE=byte-to-eat-v1-docker-consumer-orders IMAGE_NAME=byte-to-eat-docker-consumer-orders
