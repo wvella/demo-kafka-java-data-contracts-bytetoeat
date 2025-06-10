bf47afbf3fd7df41f6aa5443d85a5a06841f9e16
Warren Vella <warren.vella18@gmail.com>
2025-06-11 07:08:31 +1000

    Fix to allow Key Vaults to be deleted upon cleanup

--- terraform/confluent-cloud/main/helper-scripts/add-azure-role-assignment.sh
+++ terraform/confluent-cloud/main/helper-scripts/add-azure-role-assignment.sh
@@ -15,15 +15,21 @@ RESOURCE_GROUP="demo-data-contracts-bytetoeat-$UNIQUE_ID"
 KEYVAULT_NAME_SHARED="bte-$UNIQUE_ID-kv-shared"
 SP_NAME=("demo-data-contracts-bytetoeat-$UNIQUE_ID-tf")
 SP_ID=$(az ad sp list --display-name "$SP_NAME" --query "[0].id" -o tsv 2>/dev/null)
-if [[ -z "$SP_ID" ]]; then
-    continue
-fi
 
 # Owner User role
 az role assignment create \
     --assignee "$SP_ID" \
     --role "Owner" \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.KeyVault/vaults/$KEYVAULT_NAME_SHARED" \
+    --only-show-errors > /dev/null 2>&1
+
+# Assign Key Vault Administrator role on the Key Vault.
+# Even with the Key Vault Administrator role at the resource group level, your service principal can delete the Key Vault,
+# but reading the status of deleted vaults (to confirm deletion or for purge protection) requires the Microsoft.KeyVault/locations/deletedVaults/read permission at the subscription level.
+az role assignment create \
+    --assignee "$SP_ID" \
+    --role "Key Vault Administrator" \
+    --scope "/subscriptions/$SUBSCRIPTION_ID" \
     --only-show-errors > /dev/null 2>&1
 
 # Assign Key Vault Administrator role on the Key Vault
@@ -47,7 +53,7 @@ az role assignment create \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.KeyVault/vaults/$KEYVAULT_NAME" \
     --only-show-errors > /dev/null 2>&1
 
-    SP_NAME=("demo-data-contracts-bytetoeat-$UNIQUE_ID-java-consumer")
+SP_NAME=("demo-data-contracts-bytetoeat-$UNIQUE_ID-java-consumer")
 SP_ID=$(az ad sp list --display-name "$SP_NAME" --query "[0].id" -o tsv 2>/dev/null)
 if [[ -z "$SP_ID" ]]; then
     continue
@@ -59,6 +65,9 @@ az role assignment create \
     --role "Key Vault Crypto User" \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.KeyVault/vaults/$KEYVAULT_NAME" \
     --only-show-errors > /dev/null 2>&1
+
+# Sleep to ensure the role assignment is applied before proceeding
+sleep 60
 
 # Only output valid JSON as the last line
 echo '{"result":"Role assignments completed."}'
