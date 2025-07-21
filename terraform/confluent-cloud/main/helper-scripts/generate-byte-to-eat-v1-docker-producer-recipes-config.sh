624da9b3bf5d33403fbd97816749b5f37a8481fe
Warren Vella <warren.vella18@gmail.com>
2025-07-22 06:15:21 +1000

    Add AWS and Azure support to configuration scripts

--- terraform/confluent-cloud/main/helper-scripts/generate-byte-to-eat-v1-docker-producer-recipes-config.sh
+++ terraform/confluent-cloud/main/helper-scripts/generate-byte-to-eat-v1-docker-producer-recipes-config.sh
@@ -4,6 +4,7 @@
 TERRAFORM_DIR="../tf-working"
 PROPERTIES_TEMPLATE="../../../byte-to-eat-v1-docker-producer-recipes/producer-recipes.template"
 PROPERTIES_FILE="../../../byte-to-eat-v1-docker-producer-recipes/producer-recipes.properties"
+CLOUD=$(terraform -chdir="$TERRAFORM_DIR" output -raw cloud)
 
 # Retrieve values from Terraform outputs
 BOOTSTRAP_SERVERS=$(terraform -chdir="$TERRAFORM_DIR" output -raw kafka-url | sed 's/^SASL_SSL:\/\///')
@@ -13,9 +14,6 @@ SR_API_SECRET=$(terraform -chdir="$TERRAFORM_DIR" output -raw app-producer-schem
 KAFKA_API_KEY=$(terraform -chdir="$TERRAFORM_DIR" output -raw app-producer-kafka-api-key)
 KAFKA_API_SECRET=$(terraform -chdir="$TERRAFORM_DIR" output -raw app-producer-kafka-api-secret)
 SCHEMA_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw raw-recipes-schema-identifier)
-AZURE_TENANT_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-tenant-id)
-AZURE_CLIENT_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-java-producer-client-id)
-AZURE_CLIENT_SECRET=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-java-producer-client-secret)
 
 # Update the properties file
 sed -e "s|^bootstrap.servers=.*|bootstrap.servers=$BOOTSTRAP_SERVERS|" \
@@ -23,7 +21,19 @@ sed -e "s|^bootstrap.servers=.*|bootstrap.servers=$BOOTSTRAP_SERVERS|" \
     -e "s|^use.schema.id=.*|use.schema.id="$SCHEMA_ID"|" \
     -e "s|^sasl.jaas.config=.*|sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='$KAFKA_API_KEY' password='$KAFKA_API_SECRET';|" \
     -e "s|^schema.registry.basic.auth.user.info=.*|schema.registry.basic.auth.user.info=$SR_API_KEY:$SR_API_SECRET|" \
-    -e "s|^rule.executors._default_.param.tenant.id=.*|rule.executors._default_.param.tenant.id=$AZURE_TENANT_ID|" \
-    -e "s|^rule.executors._default_.param.client.id=.*|rule.executors._default_.param.client.id=$AZURE_CLIENT_ID|" \
-    -e "s|^rule.executors._default_.param.client.secret=.*|rule.executors._default_.param.client.secret=$AZURE_CLIENT_SECRET|" \
     "$PROPERTIES_TEMPLATE" > "$PROPERTIES_FILE"
+
+if [[ "$CLOUD" == "AZURE" ]]; then
+  TENANT_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-tenant-id)
+  CLIENT_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-java-producer-client-id)
+  CLIENT_SECRET=$(terraform -chdir="$TERRAFORM_DIR" output -raw azure-java-producer-client-secret)
+  echo "rule.executors._default_.param.tenant.id=$TENANT_ID" >> "$PROPERTIES_FILE"
+  echo "rule.executors._default_.param.client.id=$CLIENT_ID" >> "$PROPERTIES_FILE"
+  echo "rule.executors._default_.param.client.secret=$CLIENT_SECRET" >> "$PROPERTIES_FILE"
+
+elif [[ "$CLOUD" == "AWS" ]]; then
+  CLIENT_ID=$(terraform -chdir="$TERRAFORM_DIR" output -raw aws-java-producer-client-id)
+  CLIENT_SECRET=$(terraform -chdir="$TERRAFORM_DIR" output -raw aws-java-producer-client-secret)
+  echo "rule.executors._default_.param.access.key.id=$CLIENT_ID" >> "$PROPERTIES_FILE"
+  echo "rule.executors._default_.param.secret.access.key=$CLIENT_SECRET" >> "$PROPERTIES_FILE"
+fi
